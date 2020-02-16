30a940a942f7675a4b4b80b28737357b9f300f9e
JFreud <jrm.frdnbrg@gmail.com>
2020-02-15 21:59:32 -0600

    call me the piano man

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+
+my_app = Flask(__name__)
+
+
+@my_app.route("/")
+def root():
+    return render_template("home.html")
+
+@my_app.route("/recs", methods = ['GET','POST'])
+def get_recs():
+    my_ratings = request.args["my_ratings"]
+    #run model
+    return render_template("recs.html",rats=my_ratings)
+
+
+if __name__ == "__main__":
+    my_app.debug = True
+    my_app.run()
